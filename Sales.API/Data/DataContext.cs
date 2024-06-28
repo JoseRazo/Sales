7176bfa657faa31fec4d393b81976eff31fb523f
JoseRazo <joserrp.13@gmail.com>
2024-06-28 14:28:24 -0600

    Add DataContext

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; } // Countries
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
